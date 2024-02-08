4931bb415791bda315ff5fd1d02ce23972bf534d
RootGeorge <g.sopov@edu.salford.ac.uk>
2024-02-08 15:22:27 +0000

    validated movie card for invalid movies

--- assets/js/movieapi.js
+++ assets/js/movieapi.js
@@ -22,10 +22,10 @@ $(document).ready(function () {
     <h2 class="card-title currentMovie"></h2>
     <div class="movie-data poster">
       <img class="movie-poster" alt="movie-poster">
-      <h4 class="mb-1 iconTitle"> Movie title</h4>
-      <h4 class="movie-cast">movie cast</h4>
-      <h4 class="release-date">Release date</h4>
-      <h4 class="movie-rating">Movie Rating</h4>
+      <h4 class="mb-1 iconTitle"></h4>
+      <h4 class="movie-cast"></h4>
+      <h4 class="release-date"></h4>
+      <h4 class="movie-rating"></h4>
         <button class="col d-flex justify-content-center btn btn-outline-warning mt-3 addMovie" data-movie-title="${data.Title}" data-movie-poster="${data.Poster}" data-movie-actors="${data.Actors}" data-movie-released="${data.Released}" data-movie-imdb-rating="${data.imdbRating}">
         <i class="bi bi-heart-fill"></i>
           Add Movie
@@ -33,11 +33,11 @@ $(document).ready(function () {
     </div>
 `);
 
-    $(".iconTitle").text(data.Title);
-    $(".movie-poster").attr("src", data.Poster);
-    $(".movie-cast").text("Cast: " + data.Actors);
-    $(".release-date").text("Release Date: " + data.Released);
-    $(".movie-rating").text("IMDb Rating: " + data.imdbRating);
+    $(".iconTitle").text(data.Title ? data.Title : "No Movie Title Available");
+    $(".movie-poster").attr("src", data.Poster ? data.Poster : "No Movie Poster Available");
+    $(".movie-cast").text("Cast: " + data.Actors ? data.Actors : "No Movie Actors Available");
+    $(".release-date").text("Release Date: " + data.Released ? data.Released : "No Movie Release Date Available");
+    $(".movie-rating").text("IMDb Rating: " + data.imdbRating ? data.imdbRating : "No Movie imdb rating Available");
   }
 
   // Search Button Event Listener
